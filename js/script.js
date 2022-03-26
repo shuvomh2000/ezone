a58b92feadce683d1170899b38fc08bafda614f5
shuvomh2000 <shuvomh2000@gmail.com>
2022-03-26 19:39:51 +0600

    100%

--- js/script.js
+++ js/script.js
@@ -9,253 +9,247 @@ let textArrayIndex = 0;
 let charIndex = 0;
 
 function type() {
-    if (charIndex < textArray[textArrayIndex].length) {
-        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, typingDelay);
-    } else {
-        cursorSpan.classList.remove("typing");
-        setTimeout(erase, newTextDelay);
-    }
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
 }
 
 function erase() {
-    if (charIndex > 0) {
-        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
-        charIndex--;
-        setTimeout(erase, erasingDelay);
-    } else {
-        cursorSpan.classList.remove("typing");
-        textArrayIndex++;
-        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-        setTimeout(type, typingDelay + 1100);
-    }
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
-    if (textArray.length) setTimeout(type, newTextDelay + 250);
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
 $(function () {
-    'use strict'
+  'use strict'
 
-    // preloader
+  // preloader
+  $(window).load(function () {
+    $(".loader").delay(2000).fadeOut("slow");
+    $("#overlayer").delay(2000).fadeOut("slow");
 
-    $(window).load(function () {
-        // $("body").delay(2000).fadeOut();
-        $(".loader").delay(2000).fadeOut("slow");
-        $("#overlayer").delay(2000).fadeOut("slow");
+  })
 
-    })
+  // menu fixed
 
-    // menu fixed
+  var navofff = $('.nav_part').offset().top;
 
-    var navofff = $('.nav_part').offset().top;
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+    if (scrolling > navofff) {
+      $('.nav_part').addClass('menu_fixed')
+    } else {
+      $('.nav_part').removeClass('menu_fixed')
+    }
+  });
 
-    $(window).scroll(function(){
-        var scrolling = $(this).scrollTop();
-        if(scrolling > navofff){
-            $('.nav_part').addClass('menu_fixed')
-        }else{
-            $('.nav_part').removeClass('menu_fixed')
+  // count down timer
+
+  $('.main_timer').countdown('2022/05/30', function (event) {
+    // $(this).html(event.strftime('%w weeks %d days %H:%M:%S'));
+
+    $(this).html(event.strftime('<div class="single_counter">%d <span>days</span></div> <div class="single_counter">%H <span>hours</span></div> <div class="single_counter">%M <span>minutes</span></div> <div class="single_counter">%S <span>seconds</span></div>'));
+  });
+
+  // slick slider (flash sale)
+  $('.sale_slide').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false,
+    responsive: [{
+        breakpoint: 991.98,
+        settings: {
+          nextArrow: false,
+          prevArrow: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
         }
-    });
+      },
+      {
+        breakpoint: 767.98,
+        settings: {
+          nextArrow: false,
+          prevArrow: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: false
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
-    // count down timer
+  // slick slider (stream)
+  $('.stream_slide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false,
+    responsive: [{
+        breakpoint: 991.98,
+        settings: {
+          nextArrow: false,
+          prevArrow: false,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 767.98,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
-    $('.main_timer').countdown('2022/05/30', function (event) {
-        // $(this).html(event.strftime('%w weeks %d days %H:%M:%S'));
-
-        $(this).html(event.strftime('<div class="single_counter">%d <span>days</span></div> <div class="single_counter">%H <span>hours</span></div> <div class="single_counter">%M <span>minutes</span></div> <div class="single_counter">%S <span>seconds</span></div>'));
-    });
-
-    // slick slider (flash sale)
-    $('.sale_slide').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        nextArrow: false,
-        prevArrow: false,
-        responsive: [
-            {
-              breakpoint: 991.98,
-              settings: {
-                nextArrow: false,
-                prevArrow: false,
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-              }
-            },
-            {
-              breakpoint: 767.98,
-              settings: {
-                nextArrow: false,
-                prevArrow: false,
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                dots: false
-              }
-            },
-            {
-              breakpoint: 575.98,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                dots: false
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
-
-    // slick slider (stream)
-    $('.stream_slide').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        nextArrow: false,
-        prevArrow: false,
-        responsive: [
-            {
-              breakpoint: 991.98,
-              settings: {
-                nextArrow: false,
-                prevArrow: false,
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-              }
-            },
-            {
-              breakpoint: 767.98,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 575.98,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
-
-    // slick slider (product)
-    $('.product_slide').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        nextArrow: false,
-        prevArrow: false,
-        responsive: [
-            {
-              breakpoint: 991.98,
-              settings: {
-                nextArrow: false,
-                prevArrow: false,
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-              }
-            },
-            {
-              breakpoint: 767.98,
-              settings: {
-                nextArrow: false,
-                prevArrow: false,
-                slidesToShow: 3,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 575.98,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
-    // slick slider (player)
-    $('.player_slide').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        nextArrow: false,
-        prevArrow: false,
-        responsive: [
-            {
-              breakpoint: 991.98,
-              settings: {
-                nextArrow: false,
-                prevArrow: false,
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-              }
-            },
-            {
-              breakpoint: 767.98,
-              settings: {
-                nextArrow: false,
-                prevArrow: false,
-                slidesToShow: 2,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 575.98,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
+  // slick slider (product)
+  $('.product_slide').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false,
+    responsive: [{
+        breakpoint: 991.98,
+        settings: {
+          nextArrow: false,
+          prevArrow: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 767.98,
+        settings: {
+          nextArrow: false,
+          prevArrow: false,
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  // slick slider (player)
+  $('.player_slide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false,
+    responsive: [{
+        breakpoint: 991.98,
+        settings: {
+          nextArrow: false,
+          prevArrow: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 767.98,
+        settings: {
+          nextArrow: false,
+          prevArrow: false,
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
 
-    // counter part
-    $('.counter_up').counterUp();
+  // counter part
+  $('.counter_up').counterUp();
 
 
-    // back to top button
+  // back to top button
 
-  $(".bk_btn").on('click', function(){
+  $(".bk_btn").on('click', function () {
     $('html,body').animate({
       scrollTop: 0
     });
 
   });
-  $(window).on('scroll', function(){
+  $(window).on('scroll', function () {
     var abc = $(this).scrollTop();
 
-    if(abc > 150){
+    if (abc > 150) {
       $(".bk_btn").fadeIn()
-    }else{
+    } else {
       $(".bk_btn").fadeOut()
     }
 
